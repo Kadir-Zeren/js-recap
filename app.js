21d9ae55a8b5c46e96d928e0aae6e626d46b7654
kadir-zeren <k4dzeren@gmail.com>
2023-11-15 08:10:38 +0300

    variables

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 console.log("hello js");
 
-alert("this is alert");
-chose = confirm("are you sure");
+// alert("this is alert");
+// chose = confirm("are you sure");
 console.log("after alert code");
 
-console.log(chose);
+// console.log(chose);
 
 console.warn("this is a warning");
 console.error("error");
+
+const pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+
+const number1 = "3";
+console.log(typeof number1);
+
+const isOpen = true;
+console.log(isOpen, typeof isOpen);
+let language = "java";
+console.log(language, typeof language);

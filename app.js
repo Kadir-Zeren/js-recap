3629b1e2e2a5ec36ba51bbb62b1917fa07850788
kadir-zeren <k4dzeren@gmail.com>
2023-11-15 07:59:02 +0300

    recap

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("hello js");
+
+alert("this is alert");
+chose = confirm("are you sure");
+console.log("after alert code");
+
+console.log(chose);
+
+console.warn("this is a warning");
+console.error("error");

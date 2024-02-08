93999968f640841de7a03c5f931fb53171f1cda4
kadir-zeren <k4dzeren@gmail.com>
2024-02-08 12:58:02 +0300

    variables

---
+++ app.js
@@ -0,0 +1,36 @@
+console.log("local script");
+
+// alert("bu bir alert dir");
+// karar = confirm(`eminmisiniz e/h`);
+// console.log(karar);
+console.log("alert ten sonraki komut");
+
+console.warn("Bu bir uyaridir");
+
+console.error("eyvah hata olusutu");
+
+const pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+
+const sayi1 = "3";
+console.log(typeof sayi1);
+
+const varMi = true;
+const isOpen = false;
+console.log(isOpen, typeof isOpen);
+
+let language = "java";
+console.log(language, typeof language);
+
+language = "javascript";
+console.log(language);
+
+language = true;
+console.log(language);
+
+language = 3.14;
+console.log(language);
+
+let adet;
+console.log(adet);
